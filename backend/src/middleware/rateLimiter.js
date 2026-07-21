8e85522f36b6fd4489cd16c5fc50e65cc830640f
vishnusureshas <vishnuasuresh2000@gmail.com>
2026-07-21 15:27:29 +0530

    Fix: destructure RedisStore import from rate-limit-redis (v4 ESM export)

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -1,5 +1,5 @@
 const rateLimit = require('express-rate-limit');
-const RedisStore = require('rate-limit-redis');
+const { RedisStore } = require('rate-limit-redis');
 const { redis } = require('../config/redis');
 const env = require('../config/env');
 
